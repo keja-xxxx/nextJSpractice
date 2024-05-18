a27dbcc753c6cb486c8bbab4818718ab63685076
Kenneth James <kjdayapan@gmail.com>
2024-05-19 04:30:01 +0800

    Tweaking chapter 14, 15

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -18,7 +18,8 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form action={async () => {
+        <form 
+            action={async () => {
             'use server';
             await signOut();
           }}>
